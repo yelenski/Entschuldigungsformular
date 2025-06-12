1ece1c767d5a8d143b42569e6b31e5c5d8462ba6
Yelena Kunz <yelena.kunz@medelexis.ch>
2025-06-12 09:47:52 +0200

    Fix Login

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: true,
+      secure: process.env.NODE_ENV === "production",
       httpOnly: true,
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,

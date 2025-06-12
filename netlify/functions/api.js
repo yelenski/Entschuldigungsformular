944031568e5a2b9dc82d6191721e7515c391574f
Yelena Kunz <yelena.kunz@medelexis.ch>
2025-06-12 15:13:22 +0200

    app.cjs Anpassung

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,9 +1,4 @@
 const serverless = require('serverless-http');
-const express = require('express');
 const app = require('./helpers/app.cjs');
 
-// Wrapper-App, die alles unter /api mountet
-const handlerApp = express();
-handlerApp.use('/api', app);
-
-exports.handler = serverless(handlerApp);
+exports.handler = serverless(app);

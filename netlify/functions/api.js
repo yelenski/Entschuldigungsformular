bbc1acacb15a4786baed0a21c2ef34da4598aa78
Yelena Kunz <yelena.kunz@medelexis.ch>
2025-06-12 09:43:36 +0200

    Fix Login

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,4 +1,4 @@
-const serverlessExpress = require('netlify-express');
-const app = require('../../server/app'); // Passe den Pfad ggf. an
+const serverless = require('serverless-http');
+const app = require('../../server/app');
 
-exports.handler = serverlessExpress({ app });
+exports.handler = serverless(app);
